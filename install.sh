d2ce4ec65bef6a958cda39d5dd4c085198f2f6a3
Shuvo Halder <42088593+shuvo-halder@users.noreply.github.com>
2026-02-03 10:48:16 +0600

    installing package and systemd service

--- install.sh
+++ install.sh
@@ -9,5 +9,16 @@ echo "===> Installing dependencies..."
 sudo apt update
 sudo apt install -y python3 python3-pip git
 
+echo "===> Coppying files..."
+sudo rm -rf $INSTALL_DIR
+sudo mkdir -p $INSTALL_DIR
+sudo cp -r exporter.py requirements.txt $INSTALL_DIR
+
 echo "===> Installing Python Package..."
 pip3 install -r $INSTALL_DIR/requirements.txt
+
+echo "===> Installing systemd service..."
+sudo cp -r exporter.service $SERVICE_FILE
+sudo systemctl daemon-reload
+sudo systemctl enable $APP_NAME
+
